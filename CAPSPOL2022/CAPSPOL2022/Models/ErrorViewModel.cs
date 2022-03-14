a360c2de824c69c8d7cae5e0e69e90d30350a959
eduardopacompialopez <eduardopacompialopez@hotmail.com>
2022-03-14 16:48:54 -0500

    PROYECTO EN CERO CON LOGO DE INICIO 14032022

---
+++ CAPSPOL2022/CAPSPOL2022/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CAPSPOL2022.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
