ef8c259e1713f5488c56dc8c0b250ff8cb212f08
eduardopacompialopez <eduardopacompialopez@hotmail.com>
2022-03-15 17:03:37 -0500

    CONTROLADOR POSITIONS 15032022

---
+++ CAPSPOL2022/CAPSPOL2022/Controllers/PositionsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace CAPSPOL2022.Controllers
+{
+    public class PositionsController:Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ CAPSPOL2022/CAPSPOL2022/Models/Position.cs
@@ -0,0 +1,10 @@
+﻿namespace CAPSPOL2022.Models
+{
+    public class Position
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Description { get; set; }
+        public bool Flag { get; set; }
+    }
+}
